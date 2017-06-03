b84b9667fb4dd780f0fe314dca1346bfcfbd9a47
mayur kode <mayurkode7@gmail.com>
2017-06-03 21:27:16 +0530

    Modifies About Component

--- ng2-app/src/app/components/about.component.ts
+++ ng2-app/src/app/components/about.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
 
     selector:"ts-about",
-    template:'<h1> About </h1>'
+    templateUrl: './about.component.html'
 })
 
 export class AboutComponent{
